e2894c1d4a6779ec90d33316ac473d7732725a87
yurii.badger <badger.lviv.ua@gmail.com>
2022-07-26 20:32:52 +0300

    service worker being registed only on localhost and disabled user-select in PWA/standalone mode

--- packages/nx-react-pwa/src/generators/nx-react-pwa/files/src/registerServiceWorker.js
+++ packages/nx-react-pwa/src/generators/nx-react-pwa/files/src/registerServiceWorker.js
@@ -1,21 +1,42 @@
-function registerServiceWorker() {
-  const isServiceWorkerSupported = 'serviceWorker' in navigator;
+const registerServiceWorker = () => {
+  const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+      window.location.hostname === '[::1]' ||
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+  );
+
+  const isServiceWorkerSupported = 'serviceWorker' in navigator && !isLocalhost;
 
   if (!isServiceWorkerSupported) {
     console.log('Service workers are not supported.');
     return;
   }
 
-  window.addEventListener('load', function () {
+  window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js').then(
       function (registration) {
         console.log('Service worker registration succeeded:', registration);
       },
-      function (error) {
+      (error) => {
         console.log('Service worker registration failed:', error);
       }
     );
   });
-}
+};
+
+const disableUserSelect = () => {
+  const isPWA = () => {
+    return window.matchMedia('(display-mode: standalone)').matches;
+  };
+  if (!isPWA()) {
+    return;
+  }
+  document.querySelectorAll('html,body,#root').forEach((e) => {
+    e.style['user-select'] = 'none';
+  });
+};
 
 registerServiceWorker();
+disableUserSelect();
